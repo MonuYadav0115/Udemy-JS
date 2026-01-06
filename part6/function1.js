24dc1a66d0350715863d8b8d4d5d132e6b0af915
Monu Yadav <monuy7883@gmail.com>
2026-01-06 11:46:15 +0530

    js

---
+++ part6/function1.js
@@ -0,0 +1,11 @@
+
+// create a function name ordertea that takes one parameter teatype Inside this function create another function named confirmorder that return a message like order conformed for chai. 
+// call confirmOrder from within orderTea and return the result.
+
+function ordertea(teatype){
+    function confirmOrder(){
+        return "order confirm for chai"
+    } return confirmOrder()
+}
+let orderconfimation = ordertea("Lemon tea") 
+console.log(orderconfimation)

70227b8484deec8b1bd784e30711c269e2b37107
Monu Yadav <monuy7883@gmail.com>
2026-01-06 12:07:53 +0530

    js

---
+++ part6/function2.js
@@ -0,0 +1,24 @@
+// arrow function 
+
+// function greet(){}  ---->nornal function
+// const greet = ()=>{} ----->arrow function
+
+// write an arrow function named calculateTotal that takes two parameters price and quantity the function should return the total cost by multiplying the price and quantity
+// store the result in a variable totalCost:
+
+
+
+// const calculateTotal = (price,quantity) => {
+//     return price * quantity;
+// }
+// let totalCost = calculateTotal(400,20)
+// console.log(totalCost)
+
+// 2 way sortcut
+
+const calculateTotal = (price,quantity) => price * quantity;
+
+let totalCost = calculateTotal(200,10);
+
+console.log(totalCost)
+

e2665825c479ff63663e9ec1014610c59f841699
Monu Yadav <monuy7883@gmail.com>
2026-01-06 11:22:34 +0530

    js

---
+++ part6/function.js
@@ -0,0 +1,20 @@
+
+// write a function named makeTea that take one parameter typeofTea and return a string like making green tea when called with "green tea" store the result in a variable named teaOrder 
+
+// 1 
+
+
+// function makeTea(typeofTea){
+//     console.log(`Making ${typeofTea}`);
+// }
+// makeTea("green tea")
+
+
+// 2 
+
+function makeTea(typeofTea){
+    return `Making ${typeofTea}`
+}
+let teaOrder = makeTea("Green tea")
+
+console.log(teaOrder)

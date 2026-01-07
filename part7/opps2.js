2c8f9bc99f7279f589f6837944af2f808c145d4f
Monu Yadav <monuy7883@gmail.com>
2026-01-07 10:56:24 +0530

    js

---
+++ part7/opps2.js
@@ -0,0 +1,12 @@
+
+function Person(name,age,place){
+    this.name = name;
+    this.age = age,
+    this.place = place
+
+}
+let Zentrox = new Person("Zentrox tom" , 22 , "Hyderabad")
+console.log(Zentrox)
+console.log(Zentrox.name)
+console.log(Zentrox.age)
+console.log(Zentrox.place)
